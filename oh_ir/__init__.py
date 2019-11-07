6c17bd274ff42919d6d8bf41142a011f8e44a3e7
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2019-11-07 10:42:05 +0200

    periodogram into separate module

--- oh_ir/__init__.py
+++ oh_ir/__init__.py
@@ -1,7 +1,8 @@
 
-from . import io  # noqa
-from . import util  # noqa
-from . import main  # noqa
 from . import display  # noqa
+from . import io  # noqa
+from . import main  # noqa
+from . import periodogram  # noqa
+from . import util  # noqa
 
 # -fin-
